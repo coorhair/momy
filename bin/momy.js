8859892c301ac6e140a000831a1f59c7d1f3403e
Muoi Doan <muoi@vietlac.com>
2020-02-11 19:31:14 +0700

    nâng cấp thư viện MongoDB lên v3.5.x, thêm thư viện parsing CLI

--- bin/momy.js
+++ bin/momy.js
@@ -5,15 +5,45 @@
 const Tailer = require('../lib/tailer.js')
 const fs = require('fs')
 const path = require('path')
+const program = require('commander')
 
 const DEFAULT_CONFIG_PATH = 'momyfile.json'
-const refresh = process.argv.some(c => c === '--import')
-const finder = (p, c, i, a) => c === '--config' && a[i + 1] ? a[i + 1] : p
-const file = process.argv.reduce(finder, DEFAULT_CONFIG_PATH)
-const config = JSON.parse(fs.readFileSync(path.isAbsolute(file) ? file : (process.cwd() + '/' + file)))
-const tailer = new Tailer(config)
-const findCollections = (p, c, i, a) => c === '--import' && a[i + 1] ? a[i + 1] : p
-const collections = process.argv.reduce(findCollections, '')
 
-if (refresh) tailer.importAndStart(true, collections.split(',').map(c => c.trim()).filter(c => !!c))
-else tailer.start(true)
+function import_collections(value, previous) {
+  if (typeof(value) === 'string'){
+    return value.split(',').filter(x => !!x)
+  }
+  return value
+}
+
+program
+  .version('0.0.1')
+  .description('A simple cli tool for replicating MongoDB to MySQL in realtime.')
+  .option('--config <filepath>', 'path to momy mapping file', DEFAULT_CONFIG_PATH)
+  .option('--import [collections]', 'list of collection name to import, no collection provided means import all avail collections', import_collections)
+  .option('--cli-mode', 'turn on cli mode', false)
+  .option('--one-time', 'run forever or one time, by default run forever', false)
+  .parse(process.argv);
+
+// const refresh = process.argv.some(c => c === '--import')
+// const cliMode = !process.argv.some(c => c === '--service')
+// const finder = (p, c, i, a) => c === '--config' && a[i + 1] ? a[i + 1] : p
+// const file = process.argv.reduce(finder, DEFAULT_CONFIG_PATH)
+// const config = JSON.parse(fs.readFileSync(path.isAbsolute(file) ? file : (process.cwd() + '/' + file)))
+// const findCollections = (p, c, i, a) => c === '--import' && a[i + 1] ? a[i + 1] : p
+// const collections = process.argv.reduce(findCollections, '')
+
+const config = program.config
+const cliMode = program.cliMode
+const collections = program.import === true ? [] : program.import
+const refresh = !!collections
+const run_forever = !program.one_time
+
+const tailer = new Tailer(config, cliMode)
+
+if (refresh) {
+  // tailer.importAndStart(true, collections.split(',').map(c => c.trim()).filter(c => !!c))
+  tailer.importAndStart(run_forever, collections)
+} else {
+  tailer.start(run_forever)
+}
